f1619ed5efc2639744eecb6c48cb513d30f1f434
bexruz-cyber <mayoq2024@gmail.com>
2024-10-04 20:24:57 +0500

    add animation

---
+++ src/app/blog/data/dataSelectors.tsx
@@ -0,0 +1,30 @@
+export const dataSelectors = [
+    {
+        title: (
+            <>
+                 All categories
+            </>
+        ),
+    },
+    {
+        title: (
+            <>
+                 Market Insights
+            </>
+        ),
+    },
+    {
+        title: (
+            <>
+             Company News
+            </>
+        ),
+    },
+    {
+        title: (
+            <>
+             Tips and Advice
+            </>
+        ),
+    },
+]
--- src/app/blog_article/page.tsx
+++ src/app/blog_article/page.tsx
@@ -1,143 +1,28 @@
+"use client";
+
+
 import Image from "next/image";
-import headerLogo from "../../components/images/header/header_logo.svg";
 import blog_header_image from "../../components/images/blog_header.png";
 import blog_header_bg from "../../components/images/blog_header_bg.png";
 import blog_author from "../../components/images/blog_author.png";
-import header_logo from "../../components/images/header/header_logo.svg";
-import footerBg from "../../components/images/footer/footer_bg.png";
 import Link from "next/link";
 import "../../styles/home_and_blog.css"
+import { BorderGradientLight } from "shared/components/border-gradient-light";
+import { useState } from "react";
 
 export default function BlogArticle() {
+  const [hoverIndex, setHoverIndex] = useState<number | null>(null);
+  const handleMouseEnter = (index: number) => {
+    setHoverIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverIndex(null);
+  };
+
   return (
     <div className="bg-[#04030D] w-full pt-16">
-         {/* <header className="header">
-            <div className="header-conatiner items-center">
-                <Link href="/" className="header_logo_link">
-                    <Image
-                        src={header_logo}
-                        className="header_logo"
-                        alt="FCIFY Futures"
-                    />
-                </Link>
-                <ul className="header_nav_list">
-                    <li>
-                        <a href="#" className="header_nav_link">
-                            <span>0.1 /</span> Plans
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="header_nav_link">
-                            <span>0.2 /</span> How it Works
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="header_nav_link">
-                            <span>0.5 /</span> Affiliate
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="header_nav_link">
-                            <span>0.4 /</span> FAQ
-                        </a>
-                    </li>
-                </ul>
-                <a href="/" className="header_discord_link">
-                    <svg
-                        width="20"
-                        height="20"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M8.2407 8.38998C7.6707 8.38998 7.2207 8.88998 7.2207 9.49998C7.2207 10.11 7.6807 10.61 8.2407 10.61C8.8107 10.61 9.2607 10.11 9.2607 9.49998C9.2707 8.88998 8.8107 8.38998 8.2407 8.38998ZM11.8907 8.38998C11.3207 8.38998 10.8707 8.88998 10.8707 9.49998C10.8707 10.11 11.3307 10.61 11.8907 10.61C12.4607 10.61 12.9107 10.11 12.9107 9.49998C12.9107 8.88998 12.4607 8.38998 11.8907 8.38998Z"
-                            fill="#8AA4FF"
-                        />
-                        <path
-                            d="M16.7508 0H3.35078C2.22078 0 1.30078 0.92 1.30078 2.06V15.58C1.30078 16.72 2.22078 17.64 3.35078 17.64H14.6908L14.1608 15.79L15.4408 16.98L16.6508 18.1L18.8008 20V2.06C18.8008 0.92 17.8808 0 16.7508 0ZM12.8908 13.06C12.8908 13.06 12.5308 12.63 12.2308 12.25C13.5408 11.88 14.0408 11.06 14.0408 11.06C13.6308 11.33 13.2408 11.52 12.8908 11.65C12.3908 11.86 11.9108 12 11.4408 12.08C10.4808 12.26 9.60078 12.21 8.85078 12.07C8.28078 11.96 7.79078 11.8 7.38078 11.64C7.15078 11.55 6.90078 11.44 6.65078 11.3C6.62078 11.28 6.59078 11.27 6.56078 11.25C6.54078 11.24 6.53078 11.23 6.52078 11.22C6.34078 11.12 6.24078 11.05 6.24078 11.05C6.24078 11.05 6.72078 11.85 7.99078 12.23C7.69078 12.61 7.32078 13.06 7.32078 13.06C5.11078 12.99 4.27078 11.54 4.27078 11.54C4.27078 8.32 5.71078 5.71 5.71078 5.71C7.15078 4.63 8.52078 4.66 8.52078 4.66L8.62078 4.78C6.82078 5.3 5.99078 6.09 5.99078 6.09C5.99078 6.09 6.21078 5.97 6.58078 5.8C7.65078 5.33 8.50078 5.2 8.85078 5.17C8.91078 5.16 8.96078 5.15 9.02078 5.15C9.63078 5.07 10.3208 5.05 11.0408 5.13C11.9908 5.24 13.0108 5.52 14.0508 6.09C14.0508 6.09 13.2608 5.34 11.5608 4.82L11.7008 4.66C11.7008 4.66 13.0708 4.63 14.5108 5.71C14.5108 5.71 15.9508 8.32 15.9508 11.54C15.9508 11.54 15.1008 12.99 12.8908 13.06Z"
-                            fill="#8AA4FF"
-                        />
-                    </svg>
-                    Discord
-                </a>
-                <button className="header_login_btn">Login</button>
-                <button className="header_get_futured_btn">
-                    <div>
-                        Get Funded
-                        <svg
-                            width="20"
-                            height="20"
-                            viewBox="0 0 20 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M12.0254 4.94165L17.0837 9.99998L12.0254 15.0583"
-                                stroke="white"
-                                strokeWidth="1.5"
-                                strokeMiterlimit="10"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                            <path
-                                d="M2.91602 10H16.941"
-                                stroke="white"
-                                strokeWidth="1.5"
-                                strokeMiterlimit="10"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>
-                    </div>
-                    <span className="elipse">
-                        <svg
-                            width="161"
-                            height="44"
-                            viewBox="0 0 161 44"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <g filter="url(#filter0_f_1163_7608)">
-                                <ellipse
-                                    cx="78.1816"
-                                    cy="56.3232"
-                                    rx="65"
-                                    ry="27.5"
-                                    fill="#BDBBF5"
-                                />
-                            </g>
-                            <defs>
-                                <filter
-                                    id="filter0_f_1163_7608"
-                                    x="-16.8184"
-                                    y="-1.17676"
-                                    width="190"
-                                    height="115"
-                                    filterUnits="userSpaceOnUse"
-                                    colorInterpolationFilters="sRGB"
-                                >
-                                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                                    <feBlend
-                                        mode="normal"
-                                        in="SourceGraphic"
-                                        in2="BackgroundImageFix"
-                                        result="shape"
-                                    />
-                                    <feGaussianBlur
-                                        stdDeviation="15"
-                                        result="effect1_foregroundBlur_1163_7608"
-                                    />
-                                </filter>
-                            </defs>
-                        </svg>
-                    </span>
-                </button>
-            </div>
-        </header> */}
-
-      <div
-        className="bg-cover max-w-[1512px] mx-auto"
+      <div className="bg-cover max-w-[1512px] mx-auto"
         style={{
           backgroundImage: `url(${blog_header_bg.src})`,
           backgroundPosition: "center 200px",
@@ -147,7 +32,7 @@ export default function BlogArticle() {
           <Image src={blog_header_image} alt="blog header image site" />
 
           <div className="max-w-[700px] mx-auto">
-            <div className="flex items-center gap-6">
+            <div className="flex items-center gap-6 mb-8">
               <button
                 className="py-[14px] px-4 rounded-[100px] font-aventa text-[14px] leading-[17.29px]"
                 style={{
@@ -332,22 +217,8 @@ export default function BlogArticle() {
       <section className="category">
         <div className="category_container">
           <div className="category_top">
-            <h2 className="category_title">Explore by category</h2>
-            <div className="category_filter_box">
-              <button className="category_filter_btn category_filter_btn_active">
-                <span>All categories</span>
-              </button>
-              <button className="category_filter_btn">
-                <span>Market Insights</span>
-              </button>
-              <button className="category_filter_btn">
-                <span>Company News</span>
-              </button>
-              <button className="category_filter_btn">
-                <span>Tips and Advice</span>
-              </button>
-            </div>
-            <span className="category_filter_btn_line"></span>
+            <h2 className="font-normal text-[40px] max-md:text-2xl text-center">Explore by category</h2>
+            
           </div>
           <div className="category_card_box">
             {[
@@ -367,7 +238,17 @@ export default function BlogArticle() {
                 text: "Let’s look at prop firms offering similar or better services, programs...",
               },
             ].map((category, index) => (
-              <div className="category_card" key={index}>
+              <div className="category_card" key={index}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <BorderGradientLight
+                  isActive={hoverIndex === index}
+                  customStyle={{
+                    opacity: hoverIndex === index ? 1 : 0,  // Change opacity based on hover state
+                    transition: 'opacity 0.3s ease-in-out', // Smooth transition for opacity
+                  }}
+                />
                 <div className="category_card_top_box">
                   <div className="category_card_top">
                     <p className="category_info_text">Market insights</p>
@@ -386,11 +267,6 @@ export default function BlogArticle() {
               </div>
             ))}
           </div>
-          <Link href="/blog_article" className="category_bottom_btn">
-            <div>
-              <span className="category_bottom_btn_text">View more</span>
-            </div>
-          </Link>
         </div>
       </section>
 
