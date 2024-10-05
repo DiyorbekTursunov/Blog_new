ca28b5eea587ef3782a3386b4ac7fe90546d2c84
bexruz-cyber <mayoq2024@gmail.com>
2024-10-05 16:14:37 +0500

    add

--- src/app/faqs_article/page.tsx
+++ src/app/faqs_article/page.tsx
@@ -1,82 +1,49 @@
 "use client"
 
 import "../../styles/home_and_blog.css";
-import Link from 'next/link'
-import React, { useState } from 'react'
-import { BorderGradientLight } from 'shared/components/border-gradient-light'
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { BorderGradientLight } from 'shared/components/border-gradient-light';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 import './style.css';
-
-// import required modules
 import { Pagination, Navigation } from 'swiper/modules';
 
 export default function Faqs_article() {
-    const [isHover, setIsHover] = useState<boolean>(false)
+    const [isHover, setIsHover] = useState<boolean>(false);
+    const [isHover2, setIsHover2] = useState<boolean>(false);
+    const [isHover3, setIsHover3] = useState<boolean>(false);
+
     const faqs_articles = [
-        {
-            id: 1,
-            title: "Introduction to FXIFY"
-        },
-        {
-            id: 2,
-            title: "Navigating Your Dashboard"
-        },
-        {
-            id: 3,
-            title: "Finding Your Perfect Evaluation at FXIFY"
-        },
-        {
-            id: 4,
-            title: "Getting the Most Out of FXIFY Support"
-        },
-        {
-            id: 5,
-            title: "FXIFY Affiliate Program"
-        },
-        {
-            id: 6,
-            title: "Can I use someone else's card?"
-        },
-        {
-            id: 7,
-            title: "KYC/AML Policies"
-        },
-        {
-            id: 8,
-            title: "Futures Instrument List"
-        },
-        {
-            id: 9,
-            title: "Ongoing AML Monitoring"
-        },
-        {
-            id: 10,
-            title: "Ongoing KYC/Documentation Monitoring"
-        }
+        { id: 1, title: "Introduction to FXIFY" },
+        { id: 2, title: "Navigating Your Dashboard" },
+        { id: 3, title: "Finding Your Perfect Evaluation at FXIFY" },
+        { id: 4, title: "Getting the Most Out of FXIFY Support" },
+        { id: 5, title: "FXIFY Affiliate Program" },
+        { id: 6, title: "Can I use someone else's card?" },
+        { id: 7, title: "KYC/AML Policies" },
+        { id: 8, title: "Futures Instrument List" },
+        { id: 9, title: "Ongoing AML Monitoring" },
+        { id: 10, title: "Ongoing KYC/Documentation Monitoring" }
     ];
+
     const pagination = {
         clickable: true,
-        renderBullet: function (index: number, className: string): string {
-            return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
+        renderBullet: (index: number, className: string) => `<span class="${className}">${index + 1}</span>`,
     };
-
 
     return (
         <>
             <section className="hero_article pt-[148px] pb-[64px]">
                 <div className="max-w-[1148px] w-full mx-auto">
                     <div className="flex items-center gap-2 mb-[97px]">
-                        <Link href="#" className="flex items-center gap-1 font-normal text-sm  text-[#8B8FD680]">
+                        <Link href="#" className="flex items-center gap-1 font-normal text-sm text-[#8B8FD680]">
                             <span className='hero_link'>All collections</span>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M10 17L15 12L10 7" stroke="#8B8FD6" stroke-opacity="0.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M10 17L15 12L10 7" stroke="#8B8FD6" strokeOpacity="0.5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
                         </Link>
                         <Link href="#" className="font-normal text-sm text-white ">
@@ -94,8 +61,8 @@ export default function Faqs_article() {
                         >
                             <BorderGradientLight isActive={isHover}
                                 customStyle={{
-                                    opacity: isHover ? 1 : 0,  // Change opacity based on hover state
-                                    transition: 'opacity 0.3s ease-in-out', // Smooth transition for opacity
+                                    opacity: isHover ? 1 : 0,
+                                    transition: 'opacity 0.3s ease-in-out',
                                 }} />
                             <input
                                 type="search"
@@ -104,7 +71,7 @@ export default function Faqs_article() {
                                 id="hero_search"
                                 placeholder="Search for articles..."
                             />
-                            <button className="hero_search_btn">
+                            <button type="submit" className="hero_search_btn">
                                 <div>
                                     <span>Search</span>
                                     <svg
@@ -157,20 +124,9 @@ export default function Faqs_article() {
                                                 filterUnits="userSpaceOnUse"
                                                 colorInterpolationFilters="sRGB"
                                             >
-                                                <feFlood
-                                                    floodOpacity={0}
-                                                    result="BackgroundImageFix"
-                                                />
-                                                <feBlend
-                                                    mode="normal"
-                                                    in="SourceGraphic"
-                                                    in2="BackgroundImageFix"
-                                                    result="shape"
-                                                />
-                                                <feGaussianBlur
-                                                    stdDeviation={15}
-                                                    result="effect1_foregroundBlur_1163_7618"
-                                                />
+                                                <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                                                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                                                <feGaussianBlur stdDeviation={15} result="effect1_foregroundBlur_1163_7618" />
                                             </filter>
                                         </defs>
                                     </svg>
@@ -204,20 +160,9 @@ export default function Faqs_article() {
                                             filterUnits="userSpaceOnUse"
                                             colorInterpolationFilters="sRGB"
                                         >
-                                            <feFlood
-                                                floodOpacity={0}
-                                                result="BackgroundImageFix"
-                                            />
-                                            <feBlend
-                                                mode="normal"
-                                                in="SourceGraphic"
-                                                in2="BackgroundImageFix"
-                                                result="shape"
-                                            />
-                                            <feGaussianBlur
-                                                stdDeviation={40}
-                                                result="effect1_foregroundBlur_1163_7614"
-                                            />
+                                            <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                                            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                                            <feGaussianBlur stdDeviation={40} result="effect1_foregroundBlur_1163_7614" />
                                         </filter>
                                     </defs>
                                 </svg>
@@ -237,21 +182,22 @@ export default function Faqs_article() {
                         modules={[Pagination, Navigation]}
                         className="mySwiper bg-transparent"
                     >
-                        <SwiperSlide>
-                            <div className=" border border-[#1E1E2B] rounded-[40px] p-9"
-                                style={{
-                                    background: 'linear-gradient(160.99deg, rgba(255, 255, 255, 0.036) 5.45%, rgba(255, 255, 255, 0.015) 99.45%)',
-                                    backdropFilter: 'blur(40px)'
-                                }}
-                                onMouseEnter={() => setIsHover(true)}
-                                onMouseLeave={() => setIsHover(false)}
-                            >
-                                <BorderGradientLight isActive={isHover}
-                                    customStyle={{
-                                        opacity: isHover ? 1 : 0,  // Change opacity based on hover state
-                                        transition: 'opacity 0.3s ease-in-out', // Smooth transition for opacity
-                                    }} />
-                                <h2
+                        {[isHover2, isHover3].map((isHover, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="border border-[#1E1E2B] rounded-[40px] p-9"
+                                    style={{
+                                        background: 'linear-gradient(160.99deg, rgba(255, 255, 255, 0.036) 5.45%, rgba(255, 255, 255, 0.015) 99.45%)',
+                                        backdropFilter: 'blur(40px)'
+                                    }}
+                                    onMouseEnter={() => index === 0 ? setIsHover2(true) : setIsHover3(true)}
+                                    onMouseLeave={() => index === 0 ? setIsHover2(false) : setIsHover3(false)}
+                                >
+                                    <BorderGradientLight isActive={isHover}
+                                        customStyle={{
+                                            opacity: isHover ? 1 : 0,
+                                            transition: 'opacity 0.3s ease-in-out',
+                                        }} />
+                                     <h2
                                     className="text-left font-normal text-sm mb-9"
                                     style={{
                                         background: 'linear-gradient(90deg, #B9C3F4 0%, #6C728E 100%)',
@@ -274,42 +220,12 @@ export default function Faqs_article() {
                                         </li>
                                     ))}
                                 </ul>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className=" border border-[#1E1E2B] rounded-[40px] p-9"
-                                style={{
-                                    background: 'linear-gradient(160.99deg, rgba(255, 255, 255, 0.036) 5.45%, rgba(255, 255, 255, 0.015) 99.45%)',
-                                    backdropFilter: 'blur(40px)'
-                                }}>
-                                <h2
-                                    className="text-left font-normal text-sm mb-9"
-                                    style={{
-                                        background: 'linear-gradient(90deg, #B9C3F4 0%, #6C728E 100%)',
-                                        WebkitBackgroundClip: 'text',
-                                        WebkitTextFillColor: 'transparent',
-                                        backgroundClip: 'text',
-                                        color: 'transparent'
-                                    }}
-                                >Articles</h2>
-                                <ul className="flex flex-col gap-[30px]">
-                                    {faqs_articles.map(item => (
-                                        <li key={item.id} className="hover:opacity-50 transition-all duration-300">
-                                            <a href="#" className="flex items-center justify-between">
-                                                <span className="font-normal text-xl text-white">{item.title}</span>
-                                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M12.0254 4.94165L17.0837 9.99998L12.0254 15.0583" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                                    <path d="M2.91602 10H16.941" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                                </svg>
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </SwiperSlide>
+                                </div>
+                            </SwiperSlide>
+                        ))}
                     </Swiper>
                 </div>
             </section>
         </>
-    )
+    );
 }
