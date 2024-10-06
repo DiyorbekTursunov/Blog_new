ec9ce57c95db6501bad088c2e21cbedde265a1d0
bexruz-cyber <mayoq2024@gmail.com>
2024-10-06 23:08:00 +0500

    add

---
+++ src/app/privacy_disclaimer/page.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./style.css"
+
+export default function Privacy_disclaimer() {
+    return (
+        <>
+            <section className='privacy_disclaimer'>
+                <div>
+                    <h1>Disclaimer</h1>
+                    <br />
+                    <br />
+                    <br />
+                    <h3>MyFundedFutures Risk Disclosure</h3>
+                    <br />
+                    <p>
+                        Futures and forex trading contains substantial risk and is not for every investor. An investor could potentially lose all or more than the initial investment. Risk capital is money that can be lost without jeopardizing your financial security or lifestyle. Only risk capital should be used for trading and only those with <br /> sufficient risk capital should consider trading. Past performance is not necessarily indicative of future results
+                    </p>
+                    <br />
+                    <h3>Hypothetical performance disclosure</h3>
+                    <br />
+                    <p>
+                        Hypothetical performance results have many significant performance and other limitations, some of which are described below. No representation is being made that any account will or is likely to achieve profits or losses similar to those shown on the website or any promotional material.
+                    </p>
+                    <br />
+                    <h3>CFTC Rule 4.41</h3>
+                    <br />
+                    <p>
+                        Hypothetical or Simulated performance results have certain limitations. Unlike an actual performance record, simulated results do not represent actual trading. Also, because the trades have not actually been executed, the results may have under-or-over compensated for the impact, if any, of certain market factors, such as lack of liquidity.
+                        <br />
+                        <br />
+                        Simulated trading programs, in general, are also subject to the fact that they are designed with the benefit of hindsight. No representation is being made that any account will or is likely to achieve profit or losses similar to those shown.
+                        <br />
+                        <br />
+                        <br />
+                        Testimonials appearing on this website may not be representative of other clients or customers and is not a guarantee of future performance or success. No representation is being made that any evaluation account will achieve profit or losses similar to those shown here.
+                        <br />
+                        <br />
+                        All content published and distributed by MyFunded Futures LLC, and its affiliates (collectively, the “Company”) is to be treated as general information only. None of the information provided by the Company or contained herein is intended as investment advice, an offer or solicitation of an offer to buy or sell, or a recommendation, endorsement, or sponsorship of any security, company, or fund.
+                        <br />
+                        <br />
+                        Leveraged products such as CFD’s and FX trading are complex instruments with a high risk of losing money. The products offered are intended for professional and retail clients.
+                        <br />
+                        <br />
+                        Please note that client accounts could sustain losses of deposited funds or in some cases even exceeding their deposit amount. Please note; The information on the website is not directed at any jurisdiction and is not intended for any use that would be contrary to local law or regulation.
+                        <br />
+                        <br />
+                        MyFunded Futures LLC is a registered entity in Texas, USA and is compliant with all necessary laws and regulations provided in accordance with local and federal jurisdictions. Copyright © 2024. All Rights Reserved. MyFunded Futures LLC
+                    </p>
+                    <br />
+                </div>
+            </section>
+        </>
+    )
+}
